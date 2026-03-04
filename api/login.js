f8bc43eb267cfca78dd9c64456df33aee3c32c8f
Ueslei Paim <uesleibros@gmail.com>
2026-03-04 14:01:01 -0300

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,5 +1,7 @@
 import CoralApi from 'nxapi/coral';
 
+process.env.NXAPI_DATA_PATH = '/tmp';
+
 export default async function handler(req, res) {
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Method not allowed' });
